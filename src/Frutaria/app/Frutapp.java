7e870ee81ab4189af8efad07d2e141de89db61ed
LorhanPierre <lorhanpierre11102007@gmail.com>
2025-07-04 21:50:23 -0300

    LP - Inicializa o projeto
    Signed-off-by: LorhanPierre <lorhanpierre11102007@gmail.com>

--- src/Frutaria/app/Frutapp.java
+++ src/Frutaria/app/Frutapp.java
@@ -1,9 +1,18 @@
 package Frutaria.app;
 
+import Frutaria.service.Estoque;
+import Frutaria.view.Atendente;
+
 public class Frutapp {
 
     public static void main(String[]args){
+        Atendente atendente = new Atendente();
+        Estoque estoque = new Estoque();
+        int opcaoMenu = 0;
 
-        System.out.print("horse");
+        do {
+            opcaoMenu = atendente.menuPrincipal();
+            estoque.gerenciarEstoque(opcaoMenu, atendente);
+        } while (opcaoMenu != 4);
     }
 }
--- src/Frutaria/service/Estoque.java
+++ src/Frutaria/service/Estoque.java
@@ -1,4 +1,79 @@
 package Frutaria.service;
+import Frutaria.model.Fruta;
+import Frutaria.view.Atendente;
+
+import java.util.*;
 
 public class Estoque {
+
+    private List<Fruta> estoqueFruta;
+
+    public Estoque() {
+        estoqueFruta = new ArrayList<>();
+    }
+
+    public void gerenciarEstoque(int opcaoMenu, Atendente atendente) {
+
+        switch(opcaoMenu) {
+
+            case 1 -> {
+                String nome = atendente.cadastrarNome();
+                double preco = atendente.cadastrarPreco();
+                int quantidade = atendente.cadastrarQuantidade();
+                Fruta fruta = new Fruta(nome, preco, quantidade);
+                estoqueFruta.add(fruta);
+                System.out.println("\n========================================================");
+            }
+
+            case 2 -> {
+                System.out.println("\n===================== Lista de Frutas ====================\n");
+                System.out.println("----------------------------------------------------------");
+                for (Fruta fruta : estoqueFruta) {
+                    atendente.visualizar(fruta);
+                }
+
+                if(estoqueFruta.isEmpty()) {
+                    System.out.println("\nNenhuma fruta cadastrada.\n");
+                }
+            }
+
+            case 3 -> {
+                String nome = atendente.removerFruta();
+                boolean removido = false;
+
+                for (int indice = 0; indice < estoqueFruta.size(); indice++) {
+                    if (estoqueFruta.get(indice).getNome().equalsIgnoreCase(nome)) {
+                        estoqueFruta.remove(indice);
+                        removido = true;
+                        break;
+                    }
+                }
+
+                if (removido) {
+                    atendente.visualizarFrutaRemovida(nome);
+                } else {
+                    atendente.mensagemNaoEncontrada();
+                }
+            }
+
+            case 4 -> {
+                System.out.println("\nObrigado por confiar na Frutaria Bananal. Volte sempre.");
+            }
+
+            default -> {
+                System.out.println("\nNúmero inválido. Tente novamente.");
+            }
+
+        }
+
+    }
+
+    public List<Fruta> getEstoqueFruta(){
+        return estoqueFruta;
+    }
+
+    public void setEstoqueFruta(List<Fruta> estoqueFruta){
+        this.estoqueFruta = estoqueFruta;
+    }
+
 }
